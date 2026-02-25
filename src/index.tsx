78245b2ebb23aec62fd01d1875f71c88661d45b3
Yuto-ecc <2250008@ecc.ac.jp>
2026-02-26 01:24:15 +0900

    マルチプレイ用ページを追加、ルーティングを追加

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,5 @@ export { default as HomePage } from './pages/HomePage';
 export { default as MainPage } from './pages/MainPage';
 export { default as Single } from './pages/SinglePage';
 export { default as SingleGame } from './pages/SingleGame';
+export { default as SingleGame_rank } from './pages/SingleGame_rank';
 
