31d20e71e6a4f18483d791274490284c6e7b35c9
abcde <tktetsuya1226sc@gmail.com>
2026-02-28 02:30:24 +0900

    [sleep]ねむい

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -16,9 +16,9 @@ function Login() {
   return (
     <div style={{ padding: 24 }} className="min-h-screen bg-[url('/src/assets/bg.png')] bg-no-repeat bg-center bg-auto md:bg-cover">
       <Header backTo="/account"></Header>
-      <div className="w-full bg-white/80 backdrop-blur border-b border-gray-200 p-20">
+      <div className="w-2/3 bg-white/80 backdrop-blur mx-auto border-b border-gray-200 p-20">
         <div className="flex mx-auto max-w-5xl items-center justify-between px-6 py-4">
-          <button className="rounded-xl px-3 py-2 text-xl font-medium text-gray-700 hover:bg-gray-100 active:scale-75 hover:shadow-sm transition" onClick={signInWithGoogle}>
+          <button className="rounded-xl px-3 py-2 m-auto text-xl font-medium text-gray-700 hover:bg-gray-100 active:scale-75 hover:shadow-sm transition" onClick={signInWithGoogle}>
             Googleでログイン
           </button>
         </div>
